5c17a8182bcae718b6655ac1acf47c5b81e12277
nampdn <nampdn@protonmail.com>
2017-11-07 17:13:21 +0700

    test(core): add simple test runner for wallaby live debug

--- test/picpiper.spec.js
+++ test/picpiper.spec.js
@@ -1,13 +1,25 @@
 /* global describe, it, before */
 
 import chai from 'chai';
+import picpiper from '../src/index';
+import path from 'path';
 
 chai.expect();
 
 const expect = chai.expect;
 
+let dirOut, dirOne, dirTwo, pipe1, pipe2;
+
 describe('core/picpiper.js', () => {
+  before(() => {
+    dirOut = path.join(__dirname, '/dist');
+    dirOne = path.join(__dirname, '../demo/origin/source1/sub/');
+    dirTwo = path.join(__dirname, '../demo/origin/source2/sub/');
+
+    pipe1 = new picpiper.Pipe(dirOne, () => {});
+    pipe2 = new picpiper.Pipe(dirTwo, () => {});
+  });
   it('should run init test', () => {
-    expect('1').to.be.equal('1');
+    picpiper.pipe([pipe1, pipe2], dirOut);
   });
 });
