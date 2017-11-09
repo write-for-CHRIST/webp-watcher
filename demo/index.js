4c7f2f44bbd6550de5a6566119bd41ae55eb1a87
nampdn <nampdn@protonmail.com>
2017-11-09 16:13:38 +0700

    chore(demo): add simulate for demo to make real test

--- demo/index.js
+++ demo/index.js
@@ -1,11 +1,18 @@
 const path = require('path');
-const picpiper = require('../lib/pic-piper');
+
+// const picpiper = require('../lib/pic-piper');
+// const picpiper = require('../src/index');
+import picpiper from '../src/index';
 
 const dirOut = path.join(__dirname, '/dist');
 const dirOne = path.join(__dirname, '/origin/source1/');
 const dirTwo = path.join(__dirname, '/origin/source2/');
 
-const pipe1 = new picpiper.Pipe(dirOne, (path) => {});
-const pipe2 = new picpiper.Pipe(dirTwo, (path) => {});
+const pipe1 = new picpiper.Pipe(dirOne, { recursive: true }, p => {
+  return p.rel;
+});
+const pipe2 = new picpiper.Pipe(dirTwo, { recursive: true }, p => {
+  return p.rel;
+});
 
 picpiper.pipe([pipe1, pipe2], dirOut);
---
+++ demo/simulate.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+
+// Get absolute path to mock image file
+let instructorFile = path.join(__dirname, 'instructor.txt');
+
+fs.readFile(instructorFile, 'utf8', (err, content) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  let mockFile = path.join(__dirname, content);
+
+  // Generate random new name for mock image file
+  let randomName = Math.round(Math.random() * 100);
+  let relPath = path.parse(mockFile).dir;
+  let newFilePath = path.join(relPath, randomName + '.jpg');
+
+  console.log('Change file: ', mockFile, 'to: ', newFilePath);
+  fs.rename(mockFile, newFilePath, err => {
+    if (err) {
+      console.error(err);
+    } else {
+      let stripRootPath = newFilePath.substring(__dirname.length, 999);
+
+      fs.writeFile(instructorFile, stripRootPath, err => {
+        if (err) {
+          console.error(err);
+        }
+      });
+    }
+  });
+});
