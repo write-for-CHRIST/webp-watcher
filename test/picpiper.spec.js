0e594e873d13877b696c6dd01a890e9e0762cd41
nampdn <nampdn@protonmail.com>
2017-11-09 16:14:20 +0700

    test(core): add simple test for watch change

--- test/picpiper.spec.js
+++ test/picpiper.spec.js
@@ -19,7 +19,30 @@ describe('core/picpiper.js', () => {
     pipe1 = new picpiper.Pipe(dirOne, () => {});
     pipe2 = new picpiper.Pipe(dirTwo, () => {});
   });
+
   it('should run init test', () => {
     picpiper.pipe([pipe1, pipe2], dirOut);
+  });
+
+  it('should throw error not a pipe instance', () => {
+    let pp = new picpiper.PicPiper(dirOut);
+
+    expect(() => pp.addPipe('not a pipe')).to.throw('Not a pipe instance.');
+    pp.stop();
+  });
+
+  it('should throw error when piping in progress', done => {
+    let pp = new picpiper.PicPiper(dirOut);
+
+    pp.addPipe(pipe1, dirOut);
+    pp.start();
+    setTimeout(() => {
+      expect(() => pp.addPipe(new picpiper.Pipe())).to.throw(
+        'Can not add pipe after start the piping process.'
+      );
+      expect(() => pp.start()).to.throw('Piping already in progress!');
+      pp.stop();
+      done();
+    }, 100);
   });
 });
