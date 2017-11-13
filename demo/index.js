854c3a13960dc5543181f30167847ba1506882ef
nampdn <nampdn@protonmail.com>
2017-11-13 10:35:33 +0700

    test(modify test and demo file):

--- demo/index.js
+++ demo/index.js
@@ -9,10 +9,29 @@ const dirOne = path.join(__dirname, '/origin/source1/');
 const dirTwo = path.join(__dirname, '/origin/source2/');
 
 const pipe1 = new picpiper.Pipe(dirOne, { recursive: true }, p => {
-  return p.rel;
+  return {
+    dir: p.rel,
+    filename: p.name
+  };
 });
 const pipe2 = new picpiper.Pipe(dirTwo, { recursive: true }, p => {
-  return p.rel;
+  return {
+    dir: p.rel,
+    filename: p.name
+  };
 });
 
-picpiper.pipe([pipe1, pipe2], dirOut);
+picpiper.pipe([pipe1, pipe2], dirOut, {
+  convertTo: [
+    {
+      type: 'webp',
+      size: [400, 255],
+      quality: 80
+    },
+    {
+      type: 'jpeg',
+      size: [400, 255],
+      quality: 50
+    }
+  ]
+});
---
+++ test/convert.spec.js
@@ -0,0 +1,11 @@
+import chai from 'chai';
+import { Convert } from '../src/index';
+
+chai.expect();
+
+const expect = chai.expect;
+
+describe('core/convert.js', () => {
+  it('should create instance', () => {
+  });
+});
